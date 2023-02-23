cf2daafc2d785d5aa16f7c9e19c68fc876d4d416
Carlos Cáceres <carlosmicro33@gmail.com>
2023-02-23 01:04:32 -0500

    hotfix no .env

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,12 +1,10 @@
-// const { config } = require('dotenv')
-// config()
 
-const PORT = process.env.PORT || 4000
-const DB_USER = process.env.DB_HOST || 'postgres'
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_PASSWORD = process.env.DB_PASSWORD || 'pg123'
-const DB_NAME = process.env.DB_PASSWORD || 'tasksdb'
-const DB_PORT = process.env.DB_PASSWORD || '5432'
+const PORT = process.env.PORT || 4000;
+const DB_USER = process.env.DB_HOST || 'postgres';
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'pg123';
+const DB_NAME = process.env.DB_PASSWORD || 'tasksdb';
+const DB_PORT = process.env.DB_PASSWORD || '5432';
 
 module.exports = {
     db : {
